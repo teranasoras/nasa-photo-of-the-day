2fa9cb64dc71ad9a28444064c1d721ce482e2311
teranasoras <tmayn1234@gmail.com>
2020-09-11 10:00:27 -0500

    finished day 2

--- src/App.js
+++ src/App.js
@@ -5,26 +5,30 @@ import axios from 'axios';
 import TitleSection from './titlesection';
 import PictureSection from './picturesection';
 import DescriptionSection from './descriptionsection';
-
+import Loading from './loadingscreen'
 
 function App() {
   const [Picturedata, setPicturedata] = useState(dummydata);
-  // useEffect(() => {
-  //   axios.get(`https://api.nasa.gov/planetary/apod?api_key=FgjvovbIdEgMGNIHgDefY12JlDzhhyiagTnJrBW2`)
-  //     .then(res => {
-  //       setPicturedata(res.data);
-  //     })
-  //     .catch(err => {
-  //       setPicturedata(dummydata)
-  //       console.log('Failed to fetch api data')
-  //     })
-  //   }, []) 
+  const [loading, setLoading] = useState(true)
+  useEffect(() => {
+    axios.get(`https://api.nasa.gov/planetary/apod?api_key=FgjvovbIdEgMGNIHgDefY12JlDzhhyiagTnJrBW2`)
+      .then(res => {
+        setPicturedata(res.data);
+        setLoading(false);
+      })
+      .catch(err => {
+        setPicturedata(dummydata)
+        console.log('Failed to fetch api data')
+        setLoading(false);
+      })
+    }, []) 
   return (
+    loading ?(<Loading/>):(
     <div className="App">
       <TitleSection picturedata = {Picturedata}/>
       <PictureSection picturedata = {Picturedata}/>
       <DescriptionSection picturedata = {Picturedata}/>
-    </div>
+    </div>)
   );
 }
 
